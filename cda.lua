301f0a631bd9bbe6dda8667a54b03316d2e828a4
Kazuin_Exilado <109977796+KazuinExilado@users.noreply.github.com>
2023-07-16 16:29:53 -0300

    Update cda.lua

--- cda.lua
+++ cda.lua
@@ -1,5 +1,29 @@
 --bypass no anti-cheat do adonis
-loadstring(game:HttpGet'https://raw.githubusercontent.com/KazuinExilado/scripts/main/adonis%20anticheat%20bypass.lua')()
+local adonisremote
+local detectfunc
+
+for i,v in pairs(getgc(true)) do
+	if type(v) == 'table' and rawget(v, 'Detected') then
+		local f = rawget(v, 'Detected')
+
+		if typeof(f) == 'function' and islclosure(f) then
+			detectfunc = f
+		end
+	elseif type(v) == 'table' and rawget(v, 'Object')
+		and rawget(v, 'Function')
+		and rawget(v, 'FireServer')
+		and rawget(v, 'Events') then
+        adonisremote = rawget(v, 'Object')
+	end
+end
+
+hookfunction(detectfunc, function(...)
+	local args = {...}
+	if args[1] == '_' and args[2] == '_' and args[3] == true then
+		return true
+	end
+	return false
+end)
 
 --bypass no anti-cheat do cda
 local mt = getrawmetatable(game)
@@ -8,24 +32,24 @@ local hook = hookfunction or replaceclosure
 setreadonly(mt, false)
 
 function infiniteyield(this, ...)
-    return wait(9e9)
+	return wait(9e9)
 end
 
 for i, v in pairs(getnilinstances()) do
-    if v:IsA'RemoteEvent' then
-        hook(v.FireServer, infiniteyield)
-        hook(v.fireServer, infiniteyield)
-    end
+	if v:IsA'RemoteEvent' and v ~= adonisremote then
+		hook(v.FireServer, infiniteyield)
+		hook(v.fireServer, infiniteyield)
+	end
 end
 
 mt.__namecall = function(this, ...)
-    local args = {...}
+	local args = {...}
 
-    if tonumber(this.Name) then
-       return wait(9e9)
-    end
+	if tonumber(this.Name) then
+		return wait(9e9)
+	end
 
-    return namecall(this, table.unpack(args))
+	return namecall(this, table.unpack(args))
 end
 
 setreadonly(mt, true)
