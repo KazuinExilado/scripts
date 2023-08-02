da3a8c0c3693e60cf91500d1003d8efb3b29b33b
Kazuin_Exilado <109977796+KazuinExilado@users.noreply.github.com>
2023-08-02 19:24:00 -0300

    Update cda.lua

--- cda.lua
+++ cda.lua
@@ -1,8 +1,7 @@
-print'o anti cheat podre foi removido'
---[[for i, v in pairs(getsenv(game:GetService'Chat':WaitForChild'ChatService')) do
+for i, v in pairs(getsenv(game:GetService'Chat':WaitForChild'ChatService')) do
     if type(v) == 'function' and islclosure(v) then
         hookfunction(v, function()
             return wait(9e9)
         end)
     end
-end]]
+end
