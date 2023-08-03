e32c05f2dc31b25a838b90e0d5f63a97be4dab92
Kazuin_Exilado <109977796+KazuinExilado@users.noreply.github.com>
2023-08-02 22:19:19 -0300

    Update adonis anticheat bypass.lua

--- adonis anticheat bypass.lua
+++ adonis anticheat bypass.lua
@@ -14,3 +14,24 @@ for i,v in pairs(getgc(true)) do
         end
     end
 end
+
+--[[
+--in case adonis devs push this update:
+--https://cdn.discordapp.com/attachments/1085709886509301941/1136467734599843920/image.png
+
+--i did this whilst i was sleepy dont judge me
+local shit = getrenv()
+
+if not getgenv().thug  then
+	getgenv().thug = true
+	old = hookfunction(shit.debug.info, function(a, str)
+		if str == 'slanf' then
+			wait(9e9)
+			return
+		end
+		return old(a, str)
+	end)
+	print 'thug'
+end
+print 'thug'
+]]
